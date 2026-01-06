d76e04e91d61ecac18324970b2b70010cac600af
Carlos Dourado <carlosismaelad@outlook.com>
2026-01-05 21:06:05 -0300

    feat: create user serializers models

--- score/models/user.py
+++ score/models/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 from score.security import HashedPassword
+from pydantic import BaseModel, root_validator
 
 class User(SQLModel, table=True):
 
@@ -23,5 +24,35 @@ def generate_username(name: str) -> str:
   "Carlos Dourado" -> "carlos-dourado"
   """
   return name.lower().replace(" ", "-")
+
+
+class UserResponse(BaseModel):
+  """Serializer for when we send a response to the client"""
+  name: str
+  username: str
+  dept: str
+  avatar: Optional[str] = None
+  bio: Optional[str] = None
+  currency: str
+
+class UserRequest(BaseModel):
+  """Serializer for when we get the user data from the client"""
+  name: str
+  email: str
+  dept: str
+  password: str
+  username: Optional[str] = None
+  avatar: Optional[str] = None
+  bio: Optional[str] = None
+  currency: str = "USD"
+
+  @root_validator(pre=True)
+  def generate_username_if_not_set(cls, values):
+    """Generate username"""
+    if values.get("username") is None:
+      values["username"] = generate_username(values["name"])
+    return values
+
+
 
 
